8ec0479560f00f87a1a5650ff7ca5aeacf003648
David Leonard <sephirothcloud1025@yahoo.com>
2015-02-13 17:19:23 -0500

    Adding handleClick method in app.js

--- flux/src/js/components/app.js
+++ flux/src/js/components/app.js
@@ -3,8 +3,12 @@ var React = require('react');
 
 var APP = 
 	React.createClass({
+		handleClick: function(){
+			AppActions.addItem('this is the item');
+		},
+
 		render: function(){
-			return <h1> MY FLUX APP </h1>
+			return <h1 onClick={this.handleClick}> MY FLUX APP </h1>
 		}
 	});
 module.exports = APP;
